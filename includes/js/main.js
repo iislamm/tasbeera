76ead3c9bc5972edf907fb0b32da23e014cb5fd1
mostafayasser <48808351+mostafayasser@users.noreply.github.com>
2020-04-01 22:24:17 +0200

    commit_mostafa

--- includes/js/main.js
+++ includes/js/main.js
@@ -14,6 +14,37 @@ $(document).ready(function() {
 })
 
 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function increaseSlides() {
+  showSlides(slideIndex += 1);
+}
+function decreaseSlides() {
+    showSlides(slideIndex -= 1);
+  }
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+setInterval( increaseSlides, 5000);
+
+
 
 var coll = document.getElementsByClassName("collapsible");
 
